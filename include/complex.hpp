c32623b11c23791e63f1ea8757047cb1ddd7f0b0
Levina Svetlana <36290990+Vampirchik147@users.noreply.github.com>
2018-04-24 18:17:44 +0300

    Update and rename complex.hpp to include/complex.hpp

--- include/complex.hpp
+++ include/complex.hpp
@@ -32,10 +32,10 @@ public:
     Complex operator/=(const Complex&);
     Complex operator=(const Complex&);
     bool operator==(const Complex&);
-    template <class CharT, class Traits>
-    friend basic_ostream<CharT, Traits>& operator <<(basic_ostream<CharT, Traits>&, const Complex&);
-    template <class CharT, class Traits>
-    friend basic_istream<CharT, Traits>& operator >> (basic_istream<CharT, Traits>&, Complex&);
+//     template <class CharT, class Traits>
+//     friend basic_ostream<CharT, Traits>& operator <<(basic_ostream<CharT, Traits>&, const Complex&);
+//     template <class CharT, class Traits>
+//     friend basic_istream<CharT, Traits>& operator >> (basic_istream<CharT, Traits>&, Complex&);
 };
 
 #endif //LAB_6_COMPLEX_H
